7f59faaa5d6b0892127c2fceaaa3af5c9d50fb29
hardik0018 <141490030+hardik0018@users.noreply.github.com>
2024-10-21 21:05:30 +0530

    socket.io add

--- index.js
+++ index.js
@@ -38,74 +38,6 @@ app.use("/Youtube", YoutubeRoute);
 app.use("/Redeem", RedeemRoute);
 app.use("/Order", OrderRoute);
 app.use("/images", express.static("images"));
-bot.start(async (ctx) => {
-  const chatId = ctx.chat.id;
-  const username = ctx.from.username;
-  const startCommand = ctx.startPayload;
-  console.log(startCommand);
-  let refererName = "";
-
-  // Check if user already exists
-  let existingUser = await User.findOne({ teleID: chatId });
-
-  if (!existingUser) {
-    // If user does not exist, save new user
-
-    try {
-      existingUser = await User.create({
-        teleID: chatId,
-        name: username,
-        friends: [],
-        status: true,
-      });
-
-      // refer user save if new user not exist
-      if (startCommand) {
-        const referralID = startCommand;
-        const referer = await User.findOne({ teleID: referralID });
-
-        if (referer && referer.teleID !== chatId) {
-          refererName = username;
-          referer.friends.push({
-            teleID: chatId,
-            name: refererName,
-          });
-          await referer.save();
-          console.log("user save");
-        } else {
-          console.log("same user");
-        }
-      }
-    } catch (error) {
-      console.error("Error saving user:", error);
-    }
-  }
-
-  // Send a welcome message to the new user
-  let welcomeMessage = `Hi, your name is: ${username}`;
-  if (refererName) {
-    welcomeMessage += `\nYou were referred by: ${refererName}`;
-  }
-
-  ctx.reply(welcomeMessage, {
-    reply_markup: {
-      keyboard: [
-        [{ text: "web_app", web_app: { url: `${web_link}?tele=${chatId}` } }],
-      ],
-      resize_keyboard: true,
-    },
-  });
-});
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-
-bot
-  .launch()
-  .then(() => {
-    console.log("Bot is running");
-  })
-  .catch((err) => {
-    console.error("Failed to start bot:", err);
-  });
 
 const io = new socketIo.Server(server, {
   cors: {
@@ -114,8 +46,75 @@ const io = new socketIo.Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log(socket.id);
+io.on("connection", () => {
+  bot.start(async (ctx) => {
+    const chatId = ctx.chat.id;
+    const username = ctx.from.username;
+    const startCommand = ctx.startPayload;
+    console.log(startCommand);
+    let refererName = "";
+
+    // Check if user already exists
+    let existingUser = await User.findOne({ teleID: chatId });
+
+    if (!existingUser) {
+      // If user does not exist, save new user
+
+      try {
+        existingUser = await User.create({
+          teleID: chatId,
+          name: username,
+          friends: [],
+          status: true,
+        });
+
+        // refer user save if new user not exist
+        if (startCommand) {
+          const referralID = startCommand;
+          const referer = await User.findOne({ teleID: referralID });
+
+          if (referer && referer.teleID !== chatId) {
+            refererName = username;
+            referer.friends.push({
+              teleID: chatId,
+              name: refererName,
+            });
+            await referer.save();
+            console.log("user save");
+          } else {
+            console.log("same user");
+          }
+        }
+      } catch (error) {
+        console.error("Error saving user:", error);
+      }
+    }
+
+    // Send a welcome message to the new user
+    let welcomeMessage = `Hi, your name is: ${username}`;
+    if (refererName) {
+      welcomeMessage += `\nYou were referred by: ${refererName}`;
+    }
+
+    ctx.reply(welcomeMessage, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "web_app", web_app: { url: `${web_link}?tele=${chatId}` } }],
+        ],
+        resize_keyboard: true,
+      },
+    });
+  });
+  bot.help((ctx) => ctx.reply("Send me a sticker"));
+
+  bot
+    .launch()
+    .then(() => {
+      console.log("Bot is running");
+    })
+    .catch((err) => {
+      console.error("Failed to start bot:", err);
+    });
 });
 
 module.exports = app;
