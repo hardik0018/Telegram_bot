7085a8ef1d4f05d5a3c00efb7294e095bd031504
hardik0018 <141490030+hardik0018@users.noreply.github.com>
2024-10-21 20:41:56 +0530

    Add socket.io

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app = express();
-
 const { Telegraf } = require("telegraf");
 const mongoose = require("mongoose");
 const userRoute = require("./Routes/user");
 const cors = require("cors");
+const socketIo = require("socket.io");
 const User = require("./models/user");
 const MineRoute = require("./Routes/mine");
 const YoutubeRoute = require("./Routes/youtube");
 const RedeemRoute = require("./Routes/redeem");
 const OrderRoute = require("./Routes/order");
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-
 require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 4000;
+const corsConfig = {
+  origin: process.env.BASE_URL,
+  credentials: true,
+};
+
+app.use(cors(corsConfig));
 
 mongoose
   .connect(process.env.MONGODB)
@@ -23,6 +28,9 @@ mongoose
 
 const web_link = "https://telegram-game-three.vercel.app/";
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+const server = app.listen(PORT, (req, res) => {
+  console.log(`Server Listening at PORT - ${PORT}`);
+});
 
 app.use("/User", userRoute);
 app.use("/Mine", MineRoute);
@@ -99,8 +107,15 @@ bot
     console.error("Failed to start bot:", err);
   });
 
-app.listen(4000, () => {
-  console.log("server is started");
+const io = new socketIo.Server(server, {
+  cors: {
+    origin: process.env.BASE_URL,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
 });
 
 module.exports = app;
