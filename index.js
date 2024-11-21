a79ef26c8fbb38ea695e5fae58f125085506ade1
Hardik Vatukiya <hardikvatukiya0014@gmail.com>
2024-11-21 17:11:39 +0530

    add new sockte connection

--- index.js
+++ index.js
@@ -2,25 +2,27 @@ const express = require("express");
 const app = express();
 const { Telegraf } = require("telegraf");
 const mongoose = require("mongoose");
-const userRoute = require("./Routes/user");
+
 const cors = require("cors");
-const socketIo = require("socket.io");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 const User = require("./models/user");
+const userRoute = require("./Routes/user");
 const MineRoute = require("./Routes/mine");
 const YoutubeRoute = require("./Routes/youtube");
 const RedeemRoute = require("./Routes/redeem");
 const OrderRoute = require("./Routes/order");
 const AdminRoute = require("./Routes/Admin");
+
 require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 4000;
-const corsConfig = {
-  origin: process.env.BASE_URL,
-  credentials: true,
-};
 
-app.use(cors(corsConfig));
+app.use(cors());
 mongoose
   .connect(process.env.MONGODB)
   .then(() => console.log("Connected!"))
@@ -28,9 +30,6 @@ mongoose
 
 const web_link = "https://telegram-game-three.vercel.app/";
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
-const server = app.listen(PORT, (req, res) => {
-  console.log(`Server Listening at PORT - ${PORT}`);
-});
 
 app.use("/images", express.static("Images"));
 app.use("/User", userRoute);
@@ -39,15 +38,6 @@ app.use("/Youtube", YoutubeRoute);
 app.use("/Redeem", RedeemRoute);
 app.use("/Order", OrderRoute);
 app.use("/Admin", AdminRoute);
-
-const io = new socketIo.Server(server, {
-  cors: {
-    origin: process.env.BASE_URL,
-    credentials: true,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
 
 bot.start(async (ctx) => {
   const chatId = ctx.chat.id;
@@ -119,6 +109,16 @@ bot
 
 io.on("connection", (socket) => {
   console.log(socket.id);
+
+  socket.on("message", (data) => {
+    console.log(data);
+  });
+
+  socket.on("Start", (data) => {
+    console.log(data);
+  });
 });
 
-module.exports = app;
+http.listen(4000, () => {
+  console.log("Server is Running");
+});
